bd4da1679c360f32d5a9e78f114710fd01d7127c
junior8319 <tonhaun@yahoo.com.br>
2025-05-02 13:35:35 -0300

    backend-transpilation-files updated by transpilation.

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -19,12 +10,8 @@ const connection_1 = __importDefault(require("./models/connection"));
 const { personModel } = require('./models');
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use((req, res, next) => {
+app.use((req, _res, next) => {
     req.db = connection_1.default;
     next();
 });
-app.get('/people', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const [people] = yield personModel.findAll();
-    return res.status(200).json(people);
-}));
 exports.default = app;
