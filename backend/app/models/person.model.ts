2499a4f2962c1d006447b944f83604bdfed2c6cb
junior8319 <tonhaun@yahoo.com.br>
2025-05-02 13:34:36 -0300

    backend-models-set the function "findUserByName".

--- backend/app/models/person.model.ts
+++ backend/app/models/person.model.ts
@@ -3,14 +3,31 @@ import connection from "./connection";
 
 const findAll = async () => {
     const people = await connection.execute(
-        'SELECT * FROM people',
+      'SELECT * FROM people',
     );
 
     return people;
 };
 
+const findByUserName = async (userName: string) => {
+    try {
+      const person= await connection.execute(
+        `SELECT  FROM PEOPLE
+            WHERE user_name = ?
+        `,
+        [userName],
+      );
+
+      if (!person) return null;
+
+      return person;
+    } catch (error) {
+      throw new Error((error as Error).message);
+    }
+};
+
 const createNewPerson = async (person: IPerson) => {
-    
+  
 };
 
 export {
