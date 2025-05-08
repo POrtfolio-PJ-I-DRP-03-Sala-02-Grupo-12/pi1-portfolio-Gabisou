677e8a9647ac5de70af0a1c7fd5d4f3060c6a80d
Jessica Sayuri Saito <81034788+JessicaSaito@users.noreply.github.com>
2025-05-08 20:34:38 -0300

    Created Content component and css

---
+++ frontend/frontend-portfolio/src/components/Content.jsx
@@ -0,0 +1,15 @@
+import Box from "@mui/material/Box";
+import "./Content.css";
+
+const Content = () => {
+  return (
+    <>
+      <Box component="content">
+        {/* <h1>{title}</h1> 
+        {children}*/}
+      </Box>
+    </>
+  );
+};
+
+export default Content;
--- frontend/frontend-portfolio/src/pages/Home.jsx
+++ frontend/frontend-portfolio/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Box from "@mui/material/Box";
 import Header from "../components/Header";
+import Content from "../components/Content";
 
 const Home = () => {
   return (
     <>
       <Box component="home">
         <Header />
+        <Content />
       </Box>
     </>
   );
