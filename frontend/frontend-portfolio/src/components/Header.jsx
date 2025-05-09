16c2430c2d2698918f9b9eaf03fe3d3f019d2989
Jessica Sayuri Saito <81034788+JessicaSaito@users.noreply.github.com>
2025-05-08 23:58:39 -0300

    Deleted External CSS and added CSS in line

--- frontend/frontend-portfolio/src/components/Header.jsx
+++ frontend/frontend-portfolio/src/components/Header.jsx
@@ -1,11 +1,19 @@
-import "./Header.css";
 import gabisouLogo from "../assets/img/gabisoulogo.png";
 
 const Header = () => {
+  const containerStyle = {
+    backgroundColor: "#383f4d",
+    fontFamily: "Arial, Helvetica, sans-serif",
+  };
+
+  const titleStyle = {
+    color: "#FFFFFF",
+  };
+
   return (
     <>
-      <div>
-        Header
+      <div style={containerStyle}>
+        <h1 style={titleStyle}>Header</h1>
         {/* <img src="gabisouLogo" alt="Logo Empresa" />         Verificar o que deu errado*/}
       </div>
     </>
