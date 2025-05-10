0b5801aec1b6f30483da3d88df93db7a276fe2b8
Jessica Sayuri Saito <81034788+JessicaSaito@users.noreply.github.com>
2025-05-10 19:20:10 -0300

    Separated css file form jsx file on ProjectCard

--- frontend/frontend-portfolio/src/components/ProjectCard.jsx
+++ frontend/frontend-portfolio/src/components/ProjectCard.jsx
@@ -1,58 +1,26 @@
 import React, { useState } from "react";
+import "./ProjectCard.css";
 
 const ProjectCard = () => {
   const [isHovered, setIsHovered] = useState(false);
-
-  const cardStyle = {
-    width: "100%",
-    maxWidth: "350px",
-    borderRadius: "16px",
-    overflow: "hidden",
-    backgroundColor: "#ffffff",
-    transition: "transform 0.2s ease",
-    transform: isHovered ? "translateY(-4px)" : "none",
-    cursor: "pointer",
-  };
-
-  const cardImgStyle = {
-    width: "100%",
-    height: "100%",
-    objectFit: "cover",
-  };
-
-  const cardContentStyle = {
-    padding: "16px",
-  };
-
-  const cardTitleStyle = {
-    fontSize: "1.25rem",
-    fontWeight: "600",
-    margin: "0",
-    color: "#333",
-  };
-
-  const cardSubtitleStyle = {
-    fontSize: "0.95rem",
-    marginTop: "8px",
-    color: "#666",
-  };
+  const [ModalIsOpen, setModalIsOpen] = useState(false);
 
   return (
     <>
       <div
-        style={cardStyle}
+        className="card-style"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         <img
-          style={cardImgStyle}
+          className="card-img-style"
           src="https://img.itch.zone/aW1nLzU1MDU5MDMucG5n/315x250%23c/nAHQD7.png"
           alt="Project Image"
         />
 
-        <div style={cardContentStyle}>
-          <h2 style={cardTitleStyle}>Título do Projeto</h2>
-          <p style={cardSubtitleStyle}>
+        <div className="card-content-style">
+          <h2 className="card-title-style">Título do Projeto</h2>
+          <p className="card-subtitle-style">
             Subtítulo ou descrição breve do projeto que dá uma visão geral.
           </p>
         </div>
