c75309c81b658ede99482a1bf1a6607d0295b18b
junior8319 <tonhaun@yahoo.com.br>
2025-05-15 07:05:25 -0300

    feat-backend-services-set services index.

---
+++ backend/app/services/index.ts
@@ -0,0 +1,9 @@
+import * as peopleService from "./people.service";
+import * as gamesService from "./games.service";
+import * as gameImagesService from "./game.images.service";
+
+export {
+  peopleService,
+  gamesService,
+  gameImagesService,
+}
