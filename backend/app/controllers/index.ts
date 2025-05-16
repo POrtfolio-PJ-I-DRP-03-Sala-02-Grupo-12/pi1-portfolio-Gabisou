368dd9d147c22eb7196701759fc9af056bd6545d
junior8319 <tonhaun@yahoo.com.br>
2025-05-16 07:19:56 -0300

    feat-backend-controllers-add games.images controller to controllers index.

--- backend/app/controllers/index.ts
+++ backend/app/controllers/index.ts
@@ -1,7 +1,9 @@
 import * as peopleController from "./people.controller";
 import * as gamesController from "./games.controller";
+import * as gameImagesController from "./games.images.controller";
 
 export {
   peopleController,
   gamesController,
+  gameImagesController,
 };
