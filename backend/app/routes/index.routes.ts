2d002c1b34fc88d9f04183845e0592b10cbee784
junior8319 <tonhaun@yahoo.com.br>
2025-05-16 07:21:13 -0300

    feat-backend-routes-game.images: add the game.images router to the index of routes.

--- backend/app/routes/index.routes.ts
+++ backend/app/routes/index.routes.ts
@@ -1,7 +1,9 @@
 import peopleRouter from "./people.routes";
 import gamesRouter from "./games.routes";
+import gameImagesRouter from "./game.images.routes";
 
 export {
   peopleRouter,
   gamesRouter,
+  gameImagesRouter,
 };
